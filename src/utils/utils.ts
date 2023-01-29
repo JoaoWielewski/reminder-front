c4b791e889a6f46ab58c15fff071de9f448346e6
! <joaovitoras146@gmail.com>
2023-01-28 21:48:14 -0300

    Delete utils file

--- src/utils/utils.ts
+++
@@ -1,9 +0,0 @@
-export function getEnvVar(v: string): string {
-  const ret = process.env[v];
-  if (typeof ret === 'undefined') {
-      throw new Error("process.env." + v + " is undefined!");
-  }
-  return ret;
-}
-
-
