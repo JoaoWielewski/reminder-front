7e910f99b8c2f0f21b1dc5ce421eeafe0202de0b
! <joaovitoras146@gmail.com>
2023-01-29 21:13:59 -0300

    Add user registration with libraries

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -2,56 +2,71 @@
 
 import './styles.css';
 import LoginContainer from '../components/LoginContainer/page';
-import { useState } from 'react';
-import { getEnvVar } from '@/utils/utils';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+// function registerUser() {
+//   const params = {
+//     name: userName,
+//     password: userPassword,
+//   };
+//
+//   const backendUrl: string = process.env.NEXT_PUBLIC_BACKEND_URL!;
+//
+//   fetch(backendUrl + '/users', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(params),
+//   });
+// }
 
 function Signup() {
-  const [userName, setUserName] = useState('');
-  const [userPassword, setUserPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
 
-  function handleSubmit() {
-    const params = {
-      name: userName,
-      password: userPassword,
-    };
+  const schema = yup.object().shape({
+    email: yup.string().email('Email must be a valid email').max(100, 'Your email is too long').required('Email is required'),
+    password: yup.string().min(6, 'Your password must have at least 6 characters').max(20, "Your password can't have more than 20 characters").required('Password is required'),
+    confirmPassword: yup.string().oneOf([yup.ref('password'), null], "Passwords don't match").required(),
+  });
 
-    const backendUrl: string = process.env.NEXT_PUBLIC_BACKEND_URL!;
+  const { register, handleSubmit, formState: { errors } } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-    fetch(backendUrl + '/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(params),
-    });
-  }
+  const onSubmit = (data: any) => {
+    const emailInput = document.querySelector('.login-email') as HTMLInputElement;
+    emailInput.value = '';
+    const passwordInput = document.querySelector('.login-password') as HTMLInputElement;
+    passwordInput.value = '';
+    const confirmPasswordInput = document.querySelector('.login-confirm-password') as HTMLInputElement;
+    confirmPasswordInput.value = '';
+
+    console.log(data);
+  };
 
   return (
     <LoginContainer>
-      <h1 className="login-h1">Register your account</h1>
-      <div className="login-input-div">
-        <input type="text" className="login-input login-email" placeholder=" " onChange={(e) => {
-          setUserName(e.target.value);
-        }}
-        />
-        <p className="login-p login-p-email">Email</p>
-      </div>
-      <div className="login-input-div">
-        <input type="password" className="login-input login-password" placeholder=" " onChange={(e) => {
-          setUserPassword(e.target.value);
-        }}
-        />
-        <p className="login-p login-p-password">Password</p>
-      </div>
-      <div className="login-input-div">
-        <input type="password" className="login-input login-confirm-password" placeholder=" " onChange={(e) => {
-          setConfirmPassword(e.target.value);
-        }}
-        />
-        <p className="login-p login-p-confirm-password">Confirm password</p>
-      </div>
-      <button className="login-btn" onClick={handleSubmit}>Sign Up</button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1 className="login-h1">Register your account</h1>
+        <div className="login-input-div">
+          <input type="text" className="login-input login-email" placeholder=" " {...register('email')}/>
+          <p className="login-p login-p-email">Email</p>
+          <p className="error-p">{errors.email?.message?.toString()}</p>
+        </div>
+        <div className="login-input-div">
+          <input type="password" className="login-input login-password" placeholder=" " {...register('password')}/>
+          <p className="login-p login-p-password">Password</p>
+          <p className="error-p">{errors.password?.message?.toString()}</p>
+        </div>
+        <div className="login-input-div">
+          <input type="password" className="login-input login-confirm-password" placeholder=" " {...register('confirmPassword')}/>
+          <p className="login-p login-p-confirm-password">Confirm password</p>
+          <p className="error-p">{errors.confirmPassword?.message?.toString()}</p>
+        </div>
+        <button type="submit" className="login-btn">Sign Up</button>
+      </form>
     </LoginContainer>
   );
 }
