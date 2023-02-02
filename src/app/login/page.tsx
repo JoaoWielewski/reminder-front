a9cec6d247b3fa1032d5005e82c8710d5b054a0b
! <joaovitoras146@gmail.com>
2023-02-02 20:13:44 -0300

    Add NextAuth.js

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,19 +6,21 @@ import LoginContainer from '../components/LoginContainer/page';
 function Login() {
   return (
     <LoginContainer>
-      <h1 className="login-h1">Log into your account</h1>
-      <div className="login-input-div">
-        <input type="text" className="login-input login-email" placeholder=" " />
-        <p className="login-p login-p-email">Email</p>
-      </div>
-      <div className="login-input-div">
-        <input type="password" className="login-input login-password" placeholder=" " />
-        <p className="login-p login-p-password">Password</p>
-      </div>
-      <button className="login-btn">Log In</button>
-      <p className="create-account">
-        Don't have an account? <Link href="/signup">Create an account</Link>
-      </p>
+      <form>
+        <h1 className="login-h1">Log into your account</h1>
+        <div className="login-input-div">
+          <input type="text" className="login-input login-email" placeholder=" " />
+          <p className="login-p login-p-email">Email</p>
+        </div>
+        <div className="login-input-div">
+          <input type="password" className="login-input login-password" placeholder=" " />
+          <p className="login-p login-p-password">Password</p>
+        </div>
+        <button className="login-btn">Log In</button>
+        <p className="create-account">
+          Don't have an account? <Link href="/signup">Create an account</Link>
+        </p>
+      </form>
     </LoginContainer>
   );
 }
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,38 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth from "next-auth/next";
+import type { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+
+      credentials: {},
+
+      async authorize(credentials, req) {
+        const { email, password } = credentials;
+
+        const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL! + `/users/${email}/${password}`);
+
+        try {
+          const user = await res.json();
+          return user;
+        } catch {
+          return null;
+        }
+
+      }
+    })
+  ],
+
+  session: {
+    strategy: "jwt",
+  },
+
+  pages: {
+    signIn: "/login"
+  },
+
+};
+
+export default NextAuth(authOptions);
