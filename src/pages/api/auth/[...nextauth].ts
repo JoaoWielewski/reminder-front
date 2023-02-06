e24a700d864729ff02bb14dd26f3a6448b86d9d0
! <joaovitoras146@gmail.com>
2023-02-05 21:25:26 -0300

    Add jwt callback

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,7 +2,19 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import NextAuth from "next-auth/next";
 import type { NextAuthOptions } from "next-auth";
 
+type NextAuthSession = {
+  id: number;
+  email: string;
+  jwt: string;
+  expiration: number;
+}
+
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXT_AUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+    maxAge: 1 * 24 * 60 * 60,
+  },
   providers: [
     CredentialsProvider({
       name: "Credentials",
@@ -12,14 +24,20 @@ export const authOptions: NextAuthOptions = {
         password: { label: 'Password', type: 'password' },
       },
 
-      async authorize(credentials) {
-        const { email, password } = credentials;
-
-        const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL! + `/login/${email}/${password}`);
-
+      async authorize(credentials: any) {
         try {
+          //const { email, password } = credentials;
+
+          const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL! + `/login/${credentials.email}/${credentials.password}`);
           const user = await res.json();
+          const { id, email, jwt } = user;
+
+          if (!id || !email || !jwt) {
+            return null;
+          }
+
           return user;
+
         } catch {
           return null;
         }
@@ -28,12 +46,37 @@ export const authOptions: NextAuthOptions = {
     })
   ],
 
-  session: {
-    strategy: "jwt",
-  },
-
   pages: {
     signIn: "/login"
+  },
+
+  callbacks: {
+    jwt: async (token: NextAuthSession, account: NextAuthSession) => {
+      const isSignIn = !!account;
+      console.log(isSignIn);
+      const actualDateInSeconds = Math.floor(Date.now() / 1000);
+      const tokenExpirationInSeconds = (1 * 24 * 60 * 60);
+
+      if (isSignIn) {
+        if (!account || !account.id || !account.email || !account.jwt) {
+          return Promise.resolve({});
+        }
+
+        token.jwt = account.jwt;
+        token.id = account.id;
+        token.email = account.email;
+
+        token.expiration = Math.floor(actualDateInSeconds + tokenExpirationInSeconds);
+      } else {
+        if (!token?.expiration) return Promise.resolve({});
+
+        if (actualDateInSeconds > token.expiration) return Promise.resolve({});
+
+        console.log(token);
+      }
+
+      return Promise.resolve(token);
+    }
   },
 
 };
